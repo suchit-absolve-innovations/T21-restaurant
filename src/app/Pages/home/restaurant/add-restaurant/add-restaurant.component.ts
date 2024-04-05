8c52b41f703106f9a56b810a04ee04be3e275cef
Suchit Parmar <suchit.technopixar@gmail.com>
2024-04-05 16:04:13 +0530

    add restaurant

--- src/app/Pages/home/restaurant/add-restaurant/add-restaurant.component.ts
+++ src/app/Pages/home/restaurant/add-restaurant/add-restaurant.component.ts
@@ -33,31 +33,45 @@ export class AddRestaurantComponent implements OnInit {
     this.getCountry();
     this.getCountriesList();
     this.restaurantForm();
+
+    
   }
 
   restaurantForm(): void {
     this.form = this.formBuilder.group({
-      email: [''],
-      firstName: [''],
-      lastName: [''],
-      gender: [''],
-      dialCode: [+1],
-      password: [''],
-      postalCode: [''],
-      deviceType: ['web'],
-      city: [''],
-      name: [''],
-      phoneNumber: [''],
-      openingTime: [''],
-      closingTime: [''],
-      stateId: [''],
-      countryId: [231],
-      streetAddress: [''],
-      landmark: [''],
-      tipOption: [''],
-      taxApplicable: [''],
-      splitPayment: ['']
+      personalProfile: this.formBuilder.group({
+        email: [''],
+        firstName: [''],
+        lastName: [''],
+        gender: [''],
+        dialCode: ['+1'],
+        deviceType: ['web'],
+        password: [''],
+        phoneNumber: [''],
+        countryId: [231],
+        stateId: [],
+        city: [''],
+        postalCode: ['']
+      }),
+      restaurant: this.formBuilder.group({
+        name: [''],
+        openingTime: [''],
+        closingTime: [''],
+        stateId: [],
+        countryId: [231],
+        streetAddress: [''],
+        landmark: [''],
+        locationLat: [''], 
+        locationLong: [''],
+        phoneNumber: [''],
+        tipOption: [false],
+        taxApplicable: [false],
+        splitPayment: [false]
+      })
     });
+
+
+
   }
 
   onselect(event: any) {
@@ -92,40 +106,68 @@ export class AddRestaurantComponent implements OnInit {
     if (this.form.invalid) {
       return;
     }
-  
+  // Convert opening and closing time to AM/PM format
+const openingTimeAMPM = this.convertToAMPM(this.form.value.restaurant.openingTime);
+const closingTimeAMPM = this.convertToAMPM(this.form.value.restaurant.closingTime);
     const payload = {
       userId: "",
-      email: this.form.value.email,
-      firstName: this.form.value.firstName,
-      lastName: this.form.value.lastName,
-      deviceType: this.form.value.deviceType,
-      gender: this.form.value.gender,
-      dialCode: this.form.value.dialCode,
-      password: this.form.value.password,
-      postalCode: this.form.value.postalCode,
-      city: this.form.value.city,
-      name: this.form.value.name,
-      phoneNumber: this.form.value.phoneNumber,
-      openingTime: this.form.value.openingTime,
-      closingTime: this.form.value.closingTime,
-      stateId: this.form.value.stateId,
-      countryId: this.form.value.countryId,
-      streetAddress: this.form.value.streetAddress,
-      landmark: this.form.value.landmark,
-      tipOption: this.form.value.tipOption,
-      taxApplicable: this.form.value.taxApplicable,
-      splitPayment: this.form.value.splitPayment
+      personalProfile: {
+        email: this.form.value.personalProfile.email,
+        firstName: this.form.value.personalProfile.firstName,
+        lastName: this.form.value.personalProfile.lastName,
+        gender: this.form.value.personalProfile.gender,
+        deviceType: this.form.value.personalProfile.deviceType,
+        dialCode: this.form.value.personalProfile.dialCode,
+        password: this.form.value.personalProfile.password,
+        postalCode: this.form.value.personalProfile.postalCode,
+        city: this.form.value.personalProfile.city,
+        phoneNumber: this.form.value.personalProfile.phoneNumber,
+        countryId: this.form.value.personalProfile.countryId,
+        stateId: this.form.value.personalProfile.stateId
+      },
+      restaurant: {
+        name: this.form.value.restaurant.name,
+        openingTime: openingTimeAMPM, // Use the converted opening time
+        closingTime: closingTimeAMPM, // Use the converted closing time
+        stateId: this.form.value.restaurant.stateId,
+        countryId: this.form.value.restaurant.countryId,
+        streetAddress: this.form.value.restaurant.streetAddress,
+        landmark: this.form.value.restaurant.landmark,
+        locationLat: '', 
+        locationLong: '',
+        phoneNumber: this.form.value.restaurant.phoneNumber,
+        tipOption: this.form.value.restaurant.tipOption,
+        taxApplicable: this.form.value.restaurant.taxApplicable,
+        splitPayment: this.form.value.restaurant.splitPayment
+      }
     };
   
     this.restaurantService.addRestaurant(payload).subscribe(response => {
       if (response.isSuccess) {
         this.userId = response.data.userId;
         this.toaster.success(response.messages);
-      
       } else {
-      
+      this.toaster.error(response.messages)
       }
     });
+  }
+
+  convertToAMPM(time: string): string {
+    const [hours, minutes] = time.split(':');
+    let hourValue = parseInt(hours, 10);
+    let period = 'AM';
+  
+    if (hourValue >= 12) {
+      period = 'PM';
+      if (hourValue > 12) {
+        hourValue -= 12;
+      }
+    }
+  
+    // Pad single-digit hours with leading zero
+    const formattedHours = hourValue.toString().padStart(2, '0');
+  
+    return `${formattedHours}:${minutes} ${period}`;
   }
   
   getCountriesList() {
@@ -136,13 +178,14 @@ export class AddRestaurantComponent implements OnInit {
     });
   }
   getCountry() {
+    debugger
     this.contentService.getAllStates(231).subscribe((response) => {
       if (response.statusCode) {
         this.statesLists = response.data;
-        var stateListData = this.statesLists?.find((y: { stateName: any; }) => y.stateName == this.form.value.stateName);
-        this.form.patchValue({
-          stateId: stateListData?.stateId,
-        })
+        // var stateListData = this.statesLists?.find((y: { stateName: any; }) => y.stateName == this.form.value.stateName);
+        // this.form.patchValue({
+        //   stateId: stateListData?.stateId,
+        // })
       }
     });
   }
